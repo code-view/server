8675f6a1867d8a158122b8bae73ee3a7b3dbbe47
Vladimir Iakovlev <nvbn.rm@gmail.com>
2016-08-13 11:36:46 +0300

    Enable logging

--- code_view/main.py
+++ code_view/main.py
@@ -1,8 +1,11 @@
 from asyncio import get_event_loop
+from logging.config import dictConfig
 from aiohttp import web
 from .db import redis
 from .router import router
+from . import settings
 
+dictConfig(settings.LOGGING)
 app = web.Application(router=router)
 loop = get_event_loop()
 loop.run_until_complete(redis.init())
--- code_view/settings/base.py
+++ code_view/settings/base.py
@@ -1,3 +1,23 @@
 REDIS_OPTIONS = {'db': 0}
 TEST_REDIS_OPTIONS = {'db': 0}
 DEBUG = False
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'default': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'aiohttp.access': {
+            'handlers': ['default'],
+            'level': 'INFO',
+        },
+        'aiohttp.server': {
+            'handlers': ['default'],
+            'level': 'INFO',
+        },
+    }
+}
